94e86edc12b7d6f67491699a9eaae8947be0edf5
CarlangasDeveloper <cf9220543@gmail.com>
2023-09-18 15:21:47 -0500

    "Se agregó contador de puntos y se comentó el codigo"

--- MapaProyectoBlackFriday.java
+++ MapaProyectoBlackFriday.java
@@ -1,98 +1,109 @@
+import java.util.Random;
+import java.util.Scanner;
 
-    import java.util.Random;
-    import java.util.Scanner;
+public class MapaProyectoBlackFriday {
+    public static void main(String[] args) {
+        int filas = 10;
+        int columnas = 30;
+        char[][] mapa = new char[filas][columnas];
 
-    public class MapaProyectoBlackFriday {
-        public static void main(String[] args) {
-            int filas = 10;
-            int columnas = 30;
-            char[][] mapa = new char[filas][columnas];
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-            Random rand = new Random();
-            Scanner scanner = new Scanner(System.in);
+        // Generar el mapa aleatorio
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                if (i == 0 || i == filas - 1 || j == 0 || j == columnas - 1) {
+                    mapa[i][j] = '#'; // Bordes del mapa
+                } else if (rand.nextDouble() < 0.3) {
+                    mapa[i][j] = '#'; // Paredes (30% de probabilidad)
+                } else {
+                    mapa[i][j] = '.'; // Espacio en blanco (70% de probabilidad)
+                }
+            }
+        }
+        int objetosRestantes = 0;
 
+        // Generar objetos en el mapa
+        while (objetosRestantes < 5) {
+            int fila = rand.nextInt(filas);
+            int columna = rand.nextInt(columnas);
+
+            if (mapa[fila][columna] == '.') {
+                mapa[fila][columna] = '■'; // Representa un objeto al cual toca recolectar para ganar
+                objetosRestantes++;
+            }
+        }
+
+        int jugadorFila = filas / 2;
+        int jugadorColumna = columnas / 2;
+        mapa[jugadorFila][jugadorColumna] = '☻'; // Representa al jugador
+
+        int puntos = 0; // Variable para almacenar los puntos del jugador
+
+        // Bucle principal del juego
+        while (true) {
+            // Imprimir el mapa
             for (int i = 0; i < filas; i++) {
                 for (int j = 0; j < columnas; j++) {
-                    if (i == 0 || i == filas - 1 || j == 0 || j == columnas - 1) {
-                        mapa[i][j] = '#'; // Bordes
-                    } else if (rand.nextDouble() < 0.3) {
-                        mapa[i][j] = '#'; // Pared (30% de probabilidad)
-                    } else {
-                        mapa[i][j] = '.'; // Espacio en blanco (70% de probabilidad)
-                    }
+                    System.out.print(mapa[i][j]);
                 }
-            }
-            int objetosRestantes = 0;
-            while (objetosRestantes < 5) {
-                int fila = rand.nextInt(filas);
-                int columna = rand.nextInt(columnas);
-
-                if (mapa[fila][columna] == '.') {
-                    mapa[fila][columna] = '■';
-                    objetosRestantes++;
-                }
+                System.out.println();
             }
 
-            int jugadorFila = filas / 2;
-            int jugadorColumna = columnas / 2;
-            mapa[jugadorFila][jugadorColumna] = '☻';
+            System.out.println("Puntos: " + puntos); // Mostrar los puntos
 
-            while (true) {
-                for (int i = 0; i < filas; i++) {
-                    for (int j = 0; j < columnas; j++) {
-                        System.out.print(mapa[i][j]);
-                    }
-                    System.out.println();
+            System.out.println("Direccion: 1: Derecha | 2: Izquierda | 3: Arriba | 4: Abajo | 0: Salir");
+            int movimiento = scanner.nextInt();
+
+            if (movimiento == 0) {
+                break; // Salir del juego si el jugador ingresa 0
+            }
+
+            int nuevaFila = jugadorFila;
+            int nuevaColumna = jugadorColumna;
+
+            // Procesar el movimiento del jugador
+            switch (movimiento) {
+                case 1:
+                    nuevaColumna++;
+                    break;
+                case 2:
+                    nuevaColumna--;
+                    break;
+                case 3:
+                    nuevaFila--;
+                    break;
+                case 4:
+                    nuevaFila++;
+                    break;
+                default:
+                    System.out.println("Movimiento no válido.");
+                    continue;//repite el bucle despues de el default
+            }
+
+            // Verificar si el movimiento es válido y actualizar el mapa
+            if (nuevaFila >= 0 && nuevaFila < filas && nuevaColumna >= 0 && nuevaColumna < columnas && mapa[nuevaFila][nuevaColumna] != '#') {
+                if (mapa[nuevaFila][nuevaColumna] == '■') {
+                    objetosRestantes--;
+                    puntos++; // Incrementar puntos cuando se recoge un objeto
                 }
 
-                System.out.println("Direccion: 1: Derecha | 2: Izquierda | 3: Arriba | 4: Abajo | 0: Salir");
-                int movimiento = scanner.nextInt();
+                mapa[jugadorFila][jugadorColumna] = '.';
+                mapa[nuevaFila][nuevaColumna] = '☻';
+                jugadorFila = nuevaFila;
+                jugadorColumna = nuevaColumna;
 
-                if (movimiento == 0) {
+                if (objetosRestantes == 0) {
+                    System.out.println("¡Has recogido todos los objetos! Ganaste.");
                     break;
                 }
-
-                int nuevaFila = jugadorFila;
-                int nuevaColumna = jugadorColumna;
-
-                switch (movimiento) {
-                    case 1:
-                        nuevaColumna++;
-                        break;
-                    case 2:
-                        nuevaColumna--;
-                        break;
-                    case 3:
-                        nuevaFila--;
-                        break;
-                    case 4:
-                        nuevaFila++;
-                        break;
-                    default:
-                        System.out.println("Movimiento no válido.");
-                        continue;
-                }
-
-                if (nuevaFila >= 0 && nuevaFila < filas && nuevaColumna >= 0 && nuevaColumna < columnas && mapa[nuevaFila][nuevaColumna] != '#') {
-                    if (mapa[nuevaFila][nuevaColumna] == '■') {
-                        objetosRestantes--;
-                    }
-
-                    mapa[jugadorFila][jugadorColumna] = '.';
-                    mapa[nuevaFila][nuevaColumna] = '☻';
-                    jugadorFila = nuevaFila;
-                    jugadorColumna = nuevaColumna;
-
-                    if (objetosRestantes == 0) {
-                        System.out.println("¡Has recogido todos los objetos! Ganaste.");
-                        break;
-                    }
-                } else {
-                    System.out.println("Movimiento inválido.");
-                }
+            } else {
+                System.out.println("Movimiento inválido.");
             }
-
-            System.out.println("¡Juego terminado!");
         }
-    }
 
+        System.out.println("Puntos totales: " + puntos); // Mostrar los puntos totales al final del juego
+        System.out.println("¡Juego terminado!");
+    }
+}
